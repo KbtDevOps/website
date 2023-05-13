b8bb31df82e30b9328a068d1c542a1623c454726
Kong Bunthoeurn <kongbunthoeurn1@gmail.com>
2023-05-13 12:26:28 +0700

    test

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -3,6 +3,5 @@
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r ./docker-compose.yaml $WEB_SERVER_SSH_HOST:$COMPOSE_FILE_DIR/$COMPOSE_FILE_NAME
 ssh $WEB_SERVER_SSH_HOST 'docker system prune -af'
 ssh $WEB_SERVER_SSH_HOST "docker login ghcr.io -u $REGISTRY_USERNAME -p $REGISTRY_TOKEN"
-ssh $WEB_SERVER_SSH_HOST "docker compose -f $COMPOSE_FILE_DIR/$COMPOSE_FILE_NAME pull"
-ssh $WEB_SERVER_SSH_HOST "docker compose -f $COMPOSE_FILE_DIR/$COMPOSE_FILE_NAME up -d"
+ssh $WEB_SERVER_SSH_HOST "docker stack deploy --with-registry-auth --prune --resolve-image=always -c $COMPOSE_FILE_DIR/$COMPOSE_FILE_NAME node"
 sleep 15
